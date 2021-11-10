9f5d3b195c770c6cacf565ca94e16a66b2296f2d
Lakshmi Manikanteswar Punnam <lakshmi.punnam.18@mountblue.tech>
2021-11-10 17:18:18 +0530

    Added foreign_umpire_analysis_IPL.py file

---
+++ Code/foreign_umpire_analysis_IPL.py
@@ -0,0 +1,32 @@
+""" This program shows the bar chart of number of Umpires in IPL, Country wise"""
+
+# importing all the required libraries to deal with csv files and to plot the data.
+import csv
+from matplotlib import pyplot as plt
+
+# Creating the data of number of Umpires country wise, from scratch data.
+umpires = {}
+with open("IPL-Dataset-Analytics/Data/umpires.csv","r") as f:
+    reader = csv.reader(f)
+    next(reader,None) # skipping the headers
+    for row in reader:
+        country = row[1].strip()
+        if(country != "India"):
+            umpires[country] = umpires.get(country,0) + 1
+
+# Creating X-axis and Y-axis values to define the bar chart
+x_axis_values,y_axis_values = [],[]
+for country,count in umpires.items():
+    x_axis_values.append(country)
+    y_axis_values.append(count)
+
+# Creating and displaying the bar chart
+fig = plt.figure()
+fig.set_figheight(10)
+fig.set_figwidth(10)
+ax = fig.add_subplot(111)
+ax.bar(x_axis_values,y_axis_values)
+plt.title("Number of Umpires in IPL Country wise",fontweight='bold')
+plt.xlabel("Country",fontweight='bold')
+plt.ylabel("Number of Umpires in IPL",fontweight='bold')
+plt.show()
